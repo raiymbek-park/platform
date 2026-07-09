3ae0db287c8d5d00b12c2fcb2b0d1f08843108d9
sultan99 <sultan99@mail.ru>
2026-07-09 09:44:46 +0500

    refactor(shared): drop barrel re-exports orphaned by dedupe

--- apps/web/src/shared/form/index.ts
+++ apps/web/src/shared/form/index.ts
@@ -1,5 +1,4 @@
 export { inputState } from './field-state'
-export { FormDock } from './form-dock'
 export { FormScreen } from './form-screen'
 export { SearchField } from './search-field'
 export { SelectField, type SelectFieldOption } from './select-field'
