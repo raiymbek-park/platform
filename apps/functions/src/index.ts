91b910d6af3bbad1d3ecd9f72d92dca77a31bd32
sultan99 <sultan99@mail.ru>
2026-07-10 13:51:07 +0500

    feat(functions): bind ANTHROPIC_API_KEY to the api function

--- apps/functions/src/index.ts
+++ apps/functions/src/index.ts
@@ -2,6 +2,8 @@ import { appRouter, createContext } from '@raiymbek-park/api'
 import { createHTTPHandler } from '@trpc/server/adapters/standalone'
 import { onRequest } from 'firebase-functions/v2/https'
 
+import { anthropicApiKey } from './anthropic-key'
+
 export { translateIssues } from './translate-issues'
 export { translatePosts } from './translate-posts'
 
@@ -10,6 +12,11 @@ const pagesOrigin = 'https://raiymbek-park.github.io'
 const handler = createHTTPHandler({ createContext, router: appRouter })
 
 export const api = onRequest(
-  { region: 'europe-west1', minInstances: 0, cors: [pagesOrigin] },
+  {
+    region: 'europe-west1',
+    minInstances: 0,
+    cors: [pagesOrigin],
+    secrets: [anthropicApiKey],
+  },
   (req, res) => handler(req, res),
 )
